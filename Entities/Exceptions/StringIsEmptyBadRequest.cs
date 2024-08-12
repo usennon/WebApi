7a79d821ff5a59bd1cdd94d1f25223874327ead8
usennon <mikhailvorobevandr@gmail.com>
2024-08-13 02:46:34 +0300

    Feat: Exceptions

---
+++ Entities/Exceptions/StringIsEmptyBadRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+    public sealed class StringIsEmptyBadRequest : BadRequestException
+    {
+        public StringIsEmptyBadRequest() : base("String must contain characters!")
+        {
+        }
+    }
+}
