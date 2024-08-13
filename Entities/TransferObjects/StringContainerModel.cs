d3fa74944167ea126778c58b2630efa78211cf10
usennon <mikhailvorobevandr@gmail.com>
2024-08-14 01:24:08 +0300

    Transfer model for concat strings added.

---
+++ Entities/TransferObjects/StringContainerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.TransferObjects
+{
+    public class StringContainerModel
+    {
+        public string FirstString { get; init; } = "";
+        public string SecondString { get; init; } = "";
+    }
+}
