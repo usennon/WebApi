e29bf3d35290a7deb8a91b89fd6a51661a1b6626
usennon <mikhailvorobevandr@gmail.com>
2024-08-14 01:28:31 +0300

    fix: CompoundModel fix

--- Entities/TransferObjects/CompoundInterstModel.cs
+++ Entities/TransferObjects/CompoundInterstModel.cs
@@ -5,18 +5,18 @@ namespace Entities.TransferObjects
     public record CompoundInterstModel
     {
         [Range(0, int.MaxValue, ErrorMessage = "Start sum can't be below 0")]
-        public int StartSum { get; init; } = 0;
+        public int StartSum { get; set; } = 0;
 
         [Required(ErrorMessage = "Interest Rate required")]
         [Range(0, 1, ErrorMessage = "Interest rate can't be below 0 and more than 100!")]
         public double YearInterestRate { get; init; }
 
         [Range(1, 365, ErrorMessage = "In year there are only 365 days")]
-        public int NumberOfPeriods { get; init; } = 12;
+        public int NumberOfPeriods { get; set; } = 12;
 
         [Required(ErrorMessage = "You must provide necessaery amount of years.")]
         public int YearsNumber { get; init; }
 
-        public bool ifReinvestment { get; init; } = true;
+        public bool ifReinvestment { get; set; } = true;
     }
 }
