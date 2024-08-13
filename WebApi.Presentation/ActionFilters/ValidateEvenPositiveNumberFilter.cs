1a9ac0a3c8c9b8055da2b700d659550f1efc2fef
usennon <mikhailvorobevandr@gmail.com>
2024-08-13 18:10:40 +0300

    Fix: Validation DI bag

---
+++ WebApi.Presentation/ActionFilters/ValidateEvenPositiveNumberFilter.cs
@@ -0,0 +1,26 @@
+﻿using Entities.TransferObjects;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.Presentation.ActionFilters
+{
+    public class ValidateEvenPositiveNumberFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ActionArguments.TryGetValue("input", out var value))
+            {
+                if (value is IntegralParametersModel input)
+                {
+                    if (input.IntervalsAmount <= 0 || input.IntervalsAmount % 2 != 0)
+                    {
+                        context.Result = new BadRequestObjectResult(
+                            $"The number must be a positive, even integer, given {input.IntervalsAmount}.");
+                    }
+                }
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context) { }
+    }
+}
